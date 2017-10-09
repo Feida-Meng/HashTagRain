9afa5e8672e54eb476f9f23ca8bef9ba1468f3d8
codonculous <feidameng01@gmail.com>
2017-10-09 14:42:18 -0400

    Process admin input, if it contains username, convert it to twitter userId in order for twitter streaming api to work

--- server/index.js
+++ server/index.js
@@ -74,7 +74,7 @@ io.on('connection',(socket) => {
   }
 
 
-  //-----------It listens the event that user submits the hashtag----------------------------
+  //-----------listen to the event that user submits the hashtag----------------------------
   socket.on('searchHashtag', (newHashtag) => {
     userId = socket.id;
     //Sanitize the user input
@@ -93,6 +93,48 @@ io.on('connection',(socket) => {
     fetchAndPostTwit();
 
   });
+
+  // listen to filter changes made by admin
+  socket.on('adminFilterInput', async (adminFilterInput) => {
+
+    //clone the filter changes from admin,
+    //location and username in filterInput will be converted
+    //to GPS coordinates and twitter userID
+    //since Streaming API can only use filter input in those formtype
+    //the data in adminFilterInput[0] will not be converted
+    //the raw data,location and username e.g. toronto, twitter_user_name
+    // will be displayed at the admin page
+    //which is more readable than GPS coordinates and a long twitter userId
+    const filterInput = {...adminFilterInput[0]};
+
+  //--if admin input contains username, convert it to twitter id ----------------------------
+    if (adminFilterInput[0].follow) {
+      try {
+        const resp = await  T.get('users/lookup',{ screen_name: adminFilterInput[0].follow });
+        console.log(resp.data);
+
+        if ( resp.data.errors ) {
+          let warning = `User ${adminFilterInput[0].follow} cannot be found!`;
+          socket.emit('warning', warning);
+          throw warning;
+        }
+
+        filterInput.follow = resp.data[0].id_str;
+      } catch(e) {
+        console.log(e);
+        delete filterInput.follow;
+        delete adminFilterInput[0].follow;
+      }
+    }
+  //--------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
 });
 
 server.listen(PORT, () => {
