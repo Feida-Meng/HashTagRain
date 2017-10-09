c66700c92c29e6deee3c65358304f48a082ace3a
codonculous <feidameng01@gmail.com>
2017-10-09 14:18:49 -0400

    Created view for admin management page and added route GET /admin

--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ var T = new Twit({
   timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
 });
 
+app.get('/admin',(res,rep) => {
+  rep.sendFile(publicPath + 'views/admin.html');
+});
+
 app.get('/',(res,rep) => {
   rep.sendFile(publicPath + 'views/index.html');
 });
