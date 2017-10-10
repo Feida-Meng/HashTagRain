ffb2ae23994a6fcaa4c5f73b5f953165ada6b89d
codonculous <feidameng01@gmail.com>
2017-10-10 07:33:22 -0400

    Config prod_keys

--- config/prod_keys.js
+++ config/prod_keys.js
@@ -2,5 +2,6 @@ module.exports = {
   TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
   TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET,
   TWITTER_Access_Token: process.env.TWITTER_Access_Token,
-  TWITTER_Access_Token_Secret: process.env.TWITTER_Access_Token_Secret
+  TWITTER_Access_Token_Secret: process.env.TWITTER_Access_Token_Secret,
+  Admin_Login_Hash: process.env.Admin_Login_Hash
 };
